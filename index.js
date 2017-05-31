be26d7e2fed798d3db756d9468c6b60532cb78ad
Alexander <mrrandomgamerdkhd@gmail.com>
2017-05-31 17:37:02 +0200

    Basic ui up and running

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var app = express()
-var server = require('http').Server(app)
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+const PORT = Number(process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/index.html')
 })
 
 app.use('/', express.static(__dirname + '/static'))
-server.listen(2000, function () {
+
+http.listen(PORT, function () {
   console.log('Server running on port 2000');
 })
