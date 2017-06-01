bff6b511dba8e15b1a46bfe69d5a1764cc46f75a
Alexander <mrrandomgamerdkhd@gmail.com>
2017-06-01 17:51:06 +0200

    Sending now works but the anti spam timer still needs some work

--- static/js/client/ui.js
+++ static/js/client/ui.js
@@ -1,8 +1,45 @@
 var ui = {
+  buttonClicked: false,
+  handleClick: function (signal) {
+    if (!this.buttonClicked) {
+      net.sendSignal(signal)
+      this.buttonClicked = true
+      console.log('Started Timer');
+
+      setTimeout(function () {
+
+        this.buttonClicked = false;
+        console.log('Ended Timer');
+      }, 3000);
+      
+    }else if (this.buttonClicked) {
+      console.log(this.buttonClicked);
+      swal(
+        'Hov Hov...',
+        'Vent lige 20 sekunder med at ringe på igen!',
+        'error'
+      );
+    }
+  },
   constructUiFromLoadout: function (loadout) {
+    var markup = loadout
     //title
     $('.title').append(
-      $('<center><h1>'+ loadout.title +'</h1></center>')
+      $('<center><h1>'+ markup.title +'</h1></center>')
     );
+
+    //Buttons
+    for (var i = 0; i < markup.buttons.length; i+=2) {
+      $('.btnTable').append(
+        $('<tr><td><button id="'+markup.buttons[i].id+'" class="bttn-fill bttn-lg bttn-'+ markup.buttons[i].color +' doorbellbutton">'+ markup.buttons[i].name +'</button><button id="' + markup.buttons[i+1].id + '" class="bttn-fill bttn-lg bttn-'+ markup.buttons[i+1].color +' doorbellbutton">'+ markup.buttons[i+1].name +'</button></td></tr>')
+      );
+    }
+
+    //Add click events
+    markup.buttons.forEach(function (button) {
+      $('#' + button.id).click(function () {
+        ui.handleClick(button.signal)
+      })
+    })
   }
 }
