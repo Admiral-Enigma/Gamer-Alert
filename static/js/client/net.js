a133ddd7581b343f6aadf3140aba4a6fca78379a
Alexander <mrrandomgamerdkhd@gmail.com>
2017-06-01 17:49:57 +0200

    Small function to send the signals to the server

--- static/js/client/net.js
+++ static/js/client/net.js
@@ -1,6 +1,11 @@
 var socket = io()
 var gotLoadout = false
-
+var net = {
+  sendSignal: function (signal) {
+    console.log('Sending signal '+ signal);
+    socket.emit('signal', signal)
+  }
+}
 window.onload = function () {
   socket.emit('yo')
   socket.on('loadout', function (loadout) {
